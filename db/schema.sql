0f275f6ff8273757f87d86658400933e330581a4
Shaan Obney <shaanobney@Shaans-MBP.fios-router.home>
2016-10-10 09:34:51 -0700

    complete

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers (
+	id INT AUTO_INCREMENT NOT NULL,
+	burger_name VARCHAR(255) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+  	createdAt TIMESTAMP NOT NULL,
+	PRIMARY KEY(id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Manburger');
+INSERT INTO burgers (burger_name) VALUES ('Rigor Mortis Burger');
+INSERT INTO burgers (burger_name) VALUES ('Grumble Burger');
+INSERT INTO burgers (burger_name) VALUES ('Gross Burger');
+INSERT INTO burgers (burger_name) VALUES ('Good Burger');
